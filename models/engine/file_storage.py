d101262f5248dbafba07cff009d2d56b09a96f29
Gaspela04 <samirmillanorozco@hotmail.com>
2020-02-17 21:42:03 +0000

    Save, reload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -23,5 +23,18 @@ class FileStorage():
                 obj.__class__.__name__, obj.id)] = obj
 
     def save(self):
+        """ Serializes __objects = {}"""
+        new_jhoson_file = {}
+        with open(self.__file_path, mode="w") as jfile:
+            for key, value in self.__objects.items():
+                new_jhoson_file[key] = value.to_dict()
+                """JSON encoder and decoder"""
+            json.dump(new_jhoson_file, jfile)
 
     def reload(self):
+        """ Deserializes __objects = {}"""
+        if os.path.isfile(self.__file_path):
+            with open(self.__file_path, mode="r") as jfile:
+                for key, value in json.load(jfile).items():
+                    new_obj = eval(value["__class__"])(**value)
+                    self.__objects[key] = new_obj
