f2d2556e44a94fd7fb79a5fdaa9c5478233f6f2a
Gaspela04 <samirmillanorozco@hotmail.com>
2020-02-17 20:50:26 +0000

    Class all, new

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,3 +5,23 @@ Filestorage funtion all, new, save, reload
 import json
 import os.path
 from models.base_model import BaseModel
+
+
+class FileStorage():
+    """ Init"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """ Returns dict """
+        return type(self).__objects
+
+    def new(self, obj):
+        """ Sets new object in dictionary """
+        if obj:
+            self.__objects["{}.{}".format(
+                obj.__class__.__name__, obj.id)] = obj
+
+    def save(self):
+
+    def reload(self):
