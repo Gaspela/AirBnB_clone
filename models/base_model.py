42f801442873936c7c6288a393847a21951c1c42
Gaspela04 <samirmillanorozco@hotmail.com>
2020-02-17 20:19:41 +0000

    to_dict

--- models/base_model.py
+++ models/base_model.py
@@ -30,11 +30,14 @@ class BaseModel:
         self.update_at = datetime.datetime.now()
 
     def to_dict(self):
-        """Return dict"""
-        dict_copy = {}
-        dict_copy.update(self.__dict__)
-        dict_copy["__class__"] = self.__class__
-        dict_copy["update_at"] = str(datetime.datetime.isoformat(self.update_at))
-        dict_copy["created_at"] = str(datetime.datetime.isoformat(self.created_at))
-        dict_copy["id"] = str(self.id)
+        """
+        Return __dict__ value, key.
+        Convert Str to iso format.
+        serialization and deserialization.
+        """
+
+        dict_copy = self.__dict__.copy()
+        dict_copy["__class__"] = self.__class__.__name__
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
         return dict_copy
