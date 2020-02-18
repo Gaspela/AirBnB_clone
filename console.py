b3cbd0d83b5efbafddfbfb4a903d979a7751dce3
Gaspela04 <samirmillanorozco@hotmail.com>
2020-02-18 21:45:45 +0000

    Fix kevin

--- console.py
+++ console.py
@@ -22,7 +22,7 @@ class HBNBCommand(cmd.Cmd):
     """Command interpreter """
     prompt = "(hbnb) "
 
-    all_classes = {"BaseModel", "User", "State", "City",
+    classes = {"BaseModel", "User", "State", "City",
                    "Amenity", "Place", "Review"}
 
     def emptyline(self):
@@ -51,38 +51,30 @@ class HBNBCommand(cmd.Cmd):
             if not line:
                 raise SyntaxError()
             _list = line.split(" ")
-            obj = eval("{}()".format(_list[0]))
-            obj.save()
-            print("{}".format(obj.id))
+            objectsj = eval("{}()".format(_list[0]))
+            objectsj.save()
+            print("{}".format(objectsj.id))
         except SyntaxError:
             print("** class name mising **")
         except NameError:
             print("** class doesn't exist **")
 
-    def do_show(self, line):
+    def do_show(self, arg):
         """Print the str of an instance"""
-        try:
-            if not line:
-                raise SyntaxError()
-            _list = line.split(" ")
-            if _list[0] not in self.all_classes:
-                raise NameError()
-            if len(_list) < 2:
-                raise IndexError()
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in self.classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif len(args) == 2:
+            key = args[0] + "." + args[1]
             objects = storage.all()
-            key = _list[0] + '.' + _list[1]
-            if key in objects:
+            if objects.get(key):
                 print(objects[key])
             else:
-                raise KeyError()
-        except SyntaxError:
-            print("** class name missing **")
-        except NameError:
-            print("** class doesn't exist **")
-        except IndexError:
-            print("** instance id missing **")
-        except KeyError:
-            print("** no instance found **")
+                print("** no instance found **")
 
     def do_destroy(self, line):
         """Delete an instance"""
@@ -92,13 +84,13 @@ class HBNBCommand(cmd.Cmd):
             _list = line.split(" ")
             if len(_list) < 2:
                 raise IndexError()
-            objects = storage.all()
-            key = _list[0] + '.' + _list[1]
-            if key in objects:
-                del objects[key]
+            objectsjects = storage.all()
+            keyey = _list[0] + '.' + _list[1]
+            if keyey in objectsjects:
+                del objectsjects[keyey]
                 storage.save()
             else:
-                raise KeyError()
+                raise keyeyError()
 
         except SyntaxError:
             print("** class name missing **")
@@ -106,48 +98,45 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
         except IndexError:
             print("** instance id missing **")
-        except KeyError:
+        except keyeyError:
             print("** no instance found **")
 
-    def do_all(self, line):
+    def do_all(self, arg):
         """Print all str of all instances"""
         objects = storage.all()
         _list = []
-        if not line:
-            for key in objects:
-                _list.append(objects[key])
-            print(_list)
-            return
-        try:
-            args = line.split(" ")
-            if args[0] not in self.all_classes:
-                raise NameError()
-            for key in objects:
-                name = key.split('.')
-                if name[0] == args[0]:
-                    _list.append(objects[key])
-            print(_list)
-        except NameError:
-            print("** class doesn't exist **")
+        if arg:
+            if arg in self.classes:
+                for key, value in objects.items():
+                    splitkeyey = key.split(".")
+                    if splitkeyey[0] == arg:
+                        _list.append(str(value))
+            else:
+                print("** class doesn't exist **")
+        else:
+            for value in objects.values():
+                _list.append(str(value))
+
+        print(_list)
 
     def do_update(self, line):
         """Adding or updating attributes"""
         _list = line.split(" ")
         print(_list)
-        objects = storage.all()
-        print(objects[_list[0] + "." + _list[1]])
+        objectsjects = storage.all()
+        print(objectsjects[_list[0] + "." + _list[1]])
         if len(_list) < 1:
             print("** class name missing** ")
-        elif _list[0] in self.all_classes:
+        elif _list[0] in self.classes:
             if len(_list) < 2:
                 print("** instance id missing **")
-            elif _list[0] + "." + _list[1] in objects:
+            elif _list[0] + "." + _list[1] in objectsjects:
                 if len(_list) < 3:
                     print("** attribute name missing **")
                 elif len(_list) < 4:
                     print("** value missing **")
                 else:
-                    instance = objects[_list[0] + "." + _list[1]]
+                    instance = objectsjects[_list[0] + "." + _list[1]]
                     setattr(instance, _list[2], _list[3])
                     storage.save()
             else:
